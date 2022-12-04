875824179f113ad1c2ff3868dc0699cf2a585e81
plainsvillager <littlecuckoo@outlook.com>
2022-12-04 14:01:13 +0800

    add comments

--- src/main/java/cn/plainsvillager/homomod/items/foods/ModFoodComponents.java
+++ src/main/java/cn/plainsvillager/homomod/items/foods/ModFoodComponents.java
@@ -8,6 +8,7 @@ public class ModFoodComponents {
         homoの初级食物：
         吃了会减少3点饥饿值，0.7点饱食度（不吃也罢
         任何时候都能吃
+        注意HOMO_BASE_FOOD_COMPONENT里的COMPONENT
      */
     public static final FoodComponent HOMO_BASE_FOOD_COMPONENT = new FoodComponent.Builder().hunger(-3).saturationModifier(-0.7f).alwaysEdible().meat().build();
 }
